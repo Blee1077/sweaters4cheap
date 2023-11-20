2740f1237f4604c56063f7d75982991e194c61b5
blee1077 <superbrandonlee@hotmail.com>
2023-11-20 22:34:45 +0000

    Updated product URLs for correct sizing (S)

--- sweater_discount_notifier/urls.py
+++ sweater_discount_notifier/urls.py
@@ -4,9 +4,9 @@ PROD_URL_DICT = {
     'paul_james':[
         BASE_URL_PAULJAMES + "products/mens-100-cotton-fisherman-rib-knit-roll-neck-jumper?variant=41013866365120",
         BASE_URL_PAULJAMES + "products/mens-100-cotton-fisherman-rib-knit-roll-neck-jumper?variant=44210972459262",
-        BASE_URL_PAULJAMES + "products/mens-100-cotton-submariner-sweater?variant=15396735647825",
-        BASE_URL_PAULJAMES + "products/mens-100-cotton-submariner-sweater?variant=15396735680593",
-        BASE_URL_PAULJAMES + "products/mens-100-cotton-submariner-sweater?variant=15396735713361",
+        BASE_URL_PAULJAMES + "products/mens-100-cotton-submariner-sweater?variant=15396735778897",
+        BASE_URL_PAULJAMES + "products/mens-100-cotton-submariner-sweater?variant=15396735811665",
+        BASE_URL_PAULJAMES + "products/mens-100-cotton-submariner-sweater?variant=15396735844433",
         BASE_URL_PAULJAMES + "products/mens-100-cotton-fisherman-knit-jumper?variant=43655745667326",
         BASE_URL_PAULJAMES + "products/mens-100-cotton-fisherman-knit-jumper?variant=42775086825726",
         BASE_URL_PAULJAMES + "products/mens-100-british-wool-heavyweight-ribbed-jumper?variant=42775292051710",
